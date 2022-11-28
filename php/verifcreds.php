2fadfd9bdc8515009a8e8801b9c8a040a9641731
vxnxx <94240028+vxnxx@users.noreply.github.com>
2022-11-28 15:46:53 +0100

    arkadia 1.1
    integrated errors msg, register page now working

--- php/verifcreds.php
+++ php/verifcreds.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost','root','','arkadia');
+$con = mysqli_connect('localhost','root','','bddacc');
 if( $con->connect_error ) {
     die("Erreur : 1conn->connect_error");
 }
@@ -7,18 +7,20 @@ if( $con->connect_error ) {
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-$creds = ($con->real_query("SELECT `password` FROM `credentials` WHERE `username` = '$login' OR `email` = '$login'"));
+$creds = ($con->real_query("SELECT `password` FROM `bddacc` WHERE `username` = '$login' OR `email` = '$login'"));
 $result = $con->use_result();
 foreach ($result as $row) {
     $sqlPassword = $row['password'];
 }
 
+if(empty($login) || empty($password)) {
+    $isEmpty = True;
+    header('Location: http://localhost/logbdd/index.php?empty=True');
+} 
+
 if (isset($_POST['envoi'] ) ) {
-    if(mysqli_num_rows($result) != 0 && $password == $sqlPassword) {
-            header('Location: site.html');
-        } else {
-            header('Location: index.html');
-            unset($_POST['envoi']);
+    if(mysqli_num_rows($result) != 0 && $password == $sqlPassword && $isEmpty == False) {
+            header('Location: http://localhost/logbdd/site.html');
         }
     }
 ?>
