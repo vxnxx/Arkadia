f65803370fafbb96b6f41e925f4f76909e5efc16
vxnxx <VanooGaming@outlook.fr>
2022-11-29 22:19:10 +0100

    fixed error messages on the login page

--- php/register.php
+++ php/register.php
@@ -1,5 +1,5 @@
 <?php 
-$con = mysqli_connect('localhost','root','','bddacc');
+$con = mysqli_connect('localhost','root','','arkadia');
 if( $con->connect_error ) {
     die("Erreur : 1conn->connect_error");
 }
@@ -22,22 +22,22 @@ if(mysqli_num_rows($result) == 0 || ($email != $sqlEmail && $login != $sqlUserna
     $isAlreadySet = False;
 
 } else {
-    header('Location: http://localhost/logbdd/pregister.php?err=True');
+    header('Location: http://localhost/arkadia/pregister.php?err=True');
 }
 
 if(empty($login) || empty($email) || empty($passwordConf) || empty($password)) {
     $isEmpty = True;
-    header('Location: http://localhost/logbdd/pregister.php?empty=True');
+    header('Location: http://localhost/arkadia/pregister.php?empty=True');
 }
 
 if($password == $passwordConf) {
     $equalPass = True;
 } else {
-    header('Location: http://localhost/logbdd/pregister.php?notEquals=True');
+    header('Location: http://localhost/arkadia/pregister.php?notEquals=True');
 }
 
 if($isAlreadySet == False && $equalPass && $isEmpty == False) {
-    ($con->real_query("INSERT INTO `bddacc` (`id`, `username`, `email`, `password`, `grade`, `creation_date`) VALUES (NULL, '$login', '$email', '$password', '4', CURRENT_DATE());"));
-    header('Location: http://localhost/logbdd/index.php+');
+    ($con->real_query("INSERT INTO `credentials` (`id`, `username`, `email`, `password`, `grade`, `creation_date`) VALUES (NULL, '$login', '$email', '$password', '4', CURRENT_DATE());"));
+    header('Location: http://localhost/arkadia/index.php+');
 }
 ?>
